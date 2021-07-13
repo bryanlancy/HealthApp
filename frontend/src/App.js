2b37c428ca466d9d1f5843ba0eba41b8ad2b3a27
bryanlancy <bryanburns93@gmail.com>
2021-07-13 12:04:43 -0400

    convert to scss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,33 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
-import SignupFormPage from "./components/SignupFormPage";
-import * as sessionActions from "./store/session";
-import Navigation from "./components/Navigation";
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { Route, Switch } from 'react-router-dom'
+import LoginForm from './components/LoginForm'
+import SignupForm from './components/SignupForm'
+import * as sessionActions from './store/session'
+import Navigation from './components/Navigation'
 
 function App() {
-  const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState(false);
-  useEffect(() => {
-    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
-  }, [dispatch]);
+	const dispatch = useDispatch()
+	const [isLoaded, setIsLoaded] = useState(false)
+	useEffect(() => {
+		dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true))
+	}, [dispatch])
 
-  return (
-    <>
-      <Navigation isLoaded={isLoaded} />
-      {isLoaded && (
-        <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route>
-        </Switch>
-      )}
-    </>
-  );
+	return (
+		<>
+			<Navigation isLoaded={isLoaded} />
+			{isLoaded && (
+				<Switch>
+					<Route path="/login">
+						<LoginForm />
+					</Route>
+					<Route path="/signup">
+						<SignupForm />
+					</Route>
+				</Switch>
+			)}
+		</>
+	)
 }
 
-export default App;
+export default App
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,34 +1,31 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
-import ProfileButton from "./ProfileButton";
-import "./Navigation.css";
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import ProfileButton from './ProfileButton'
 
 function Navigation({ isLoaded }) {
-  const sessionUser = useSelector((state) => state.session.user);
+	const sessionUser = useSelector(state => state.session.user)
 
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = <ProfileButton user={sessionUser} />;
-  } else {
-    sessionLinks = (
-      <>
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
-      </>
-    );
-  }
+	let sessionLinks
+	if (sessionUser) {
+		sessionLinks = <ProfileButton user={sessionUser} />
+	} else {
+		sessionLinks = (
+			<>
+				<NavLink to="/login">Log In</NavLink>
+				<NavLink to="/signup">Sign Up</NavLink>
+			</>
+		)
+	}
 
-  return (
-    <ul>
-      <li>
-        <NavLink exact to="/">
-          Home
-        </NavLink>
-        {isLoaded && sessionLinks}
-      </li>
-    </ul>
-  );
+	return (
+		<nav className="navigation">
+			<NavLink exact to="/">
+				Home
+			</NavLink>
+			{isLoaded && sessionLinks}
+		</nav>
+	)
 }
 
-export default Navigation;
+export default Navigation
