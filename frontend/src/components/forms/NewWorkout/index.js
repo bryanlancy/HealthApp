b63403ecb2b9caf7ebba6b14bf9f15ed84c0b558
bryanlancy <bryanburns93@gmail.com>
2021-07-14 01:24:31 -0400

    add redux

--- frontend/src/components/forms/NewWorkout/index.js
+++ frontend/src/components/forms/NewWorkout/index.js
@@ -1,26 +1,28 @@
-function NewWorkout() {
-  return (
-    <form className="new-workout-form">
-      <h2>Add New Workout</h2>
-      <label htmlFor="">
-        Exercise
-        <input type="text" list="exercise" />
-        <datalist id="exercise">
-          <option value="HARDCODE Pushups" />
-          <option value="HARDCODE Pullups" />
-        </datalist>
-      </label>
-      <label htmlFor="">
-        Reps
-        <input type="text" />
-      </label>
-      <label htmlFor="">
-        MET Value of this workout
-        <input type="text" />
-      </label>
-      <button type="submit">Add Workout</button>
-    </form>
-  );
-}
+import { useSelector } from 'react-redux'
 
-export default NewWorkout;
+export default function NewWorkout() {
+	const categories = useSelector(state => state.exercise)
+	console.log()
+	return (
+		<form className="new-workout-form">
+			<h2>Add New Workout</h2>
+			<label htmlFor="">
+				Exercise
+				<input type="text" list="exercise" />
+				<datalist id="exercise">
+					<option value="HARDCODE Pushups" />
+					<option value="HARDCODE Pullups" />
+				</datalist>
+			</label>
+			<label htmlFor="">
+				Reps
+				<input type="text" />
+			</label>
+			<label htmlFor="">
+				MET Value of this workout
+				<input type="text" />
+			</label>
+			<button type="submit">Add Workout</button>
+		</form>
+	)
+}
