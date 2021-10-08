41c5c7f55461256c7a74be1999e4fc46edc78621
bryanlancy <bryanburns93@gmail.com>
2021-10-07 21:33:28 -0400

    start grouping workouts

--- frontend/src/components/WorkoutGroupCreator/index.js
+++ frontend/src/components/WorkoutGroupCreator/index.js
@@ -2,6 +2,13 @@ import { useEffect, useMemo } from "react";
 import { useSelector } from "react-redux";
 import WorkoutGroup from "./WorkoutGroup";
 
+
+// [4,7,6,5,3,1,2,8,9] //? Sorted (date) Workout Ids
+// [1,1,1,2,2,3,4,4,4] //? Workout Date
+
+
+
+
 export default function WorkoutGroupCreator({ allWorkouts }) {
 
 
